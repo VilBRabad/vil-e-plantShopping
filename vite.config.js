b866c396453541c8e1cdb0665b7fdda1ea76c61b
VilBRabad <rabadvilas11@gmail.com>
2024-10-17 01:48:47 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/vil-e-plantShopping",
+  base: "/vil-e-plantShopping/",
   plugins: [react()],
 });
